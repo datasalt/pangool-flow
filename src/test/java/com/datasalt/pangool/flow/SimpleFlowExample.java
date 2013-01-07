a28058ecc7c90ed9269a645989e31f181dcecd2e
pere <pere@datasalt.com>
2013-01-07 10:56:10 +0100

    Will add some examples soon.

---
+++ src/test/java/com/datasalt/pangool/flow/SimpleFlowExample.java
@@ -0,0 +1,14 @@
+package com.datasalt.pangool.flow;
+
+import org.junit.Test;
+
+public class SimpleFlowExample {
+
+	@Test
+	public void test() {
+		
+		MapReduceFlowBuilder builder = new MapReduceFlowBuilder();
+
+		
+	}
+}
